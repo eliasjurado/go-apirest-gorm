7c0fa542effb0d2c18868377474677186df50070
Elias Jurado <ridimdev@gmail.com>
2024-03-27 18:29:47 -0500

    clean main.go

--- main.go
+++ main.go
@@ -24,29 +24,4 @@ func main() {
 	mux.HandleFunc("/api/user/{id:[0-9]+}", handlers.RemoveUser).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":3000", mux))
-
-	// database.Connect()
-
-	// //Create tables
-	// // models.Init(database.DB)
-	// database.CreateTable(models.UserSchema, "user")
-
-	// // database.TruncateTable("users")
-
-	// //Create
-	// // user := models.User{Username: "pretelcarla", Password:"1234567689", Email:"pretel.carla@gmail.com"}
-	// // user.Save(database.DB)
-
-	// //Update
-	// // user := models.User{Id: 1,Username: "pretelcarla", Password:"1234567689", Email:"pretel.carla@gmail.com"}
-	// // user.Save(database.DB)
-
-	// //Delete
-	// // user := models.User{Id: 1}
-	// // user.Delete(database.DB)
-
-	// log.Printf("%+v\n", models.ListUsers(database.DB))
-	// log.Printf("%+v\n", models.GetUser(database.DB, 1))
-	// defer database.Close()
-
 }
